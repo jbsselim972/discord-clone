283ac44274015e524d3c447515dc071f66116b1c
jbsselim <jbsselim@gmail.com>
2024-01-10 13:54:02 -0400

    changes

--- next.config.js
+++ next.config.js
@@ -5,15 +5,11 @@ const nextConfig = {
       "utf-8-validate": "commonjs utf-8-validate",
       bufferutil: "commonjs bufferutil",
     });
+
     return config;
   },
   images: {
-    remotePatterns: [
-      {
-        hostname: "utfs.io",
-        protocol: "https",
-      },
-    ],
+    domains: ["uploadthing.com", "utfs.io"],
   },
 };
 
