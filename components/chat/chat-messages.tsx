1e8611eef104acb6c162b9f4a55dcfa8db6f1a5e
jbsselim <jbsselim@gmail.com>
2024-01-10 11:30:15 -0400

    changes

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -65,7 +65,7 @@ export const ChatMessages = ({
     count: data?.pages?.[0]?.items?.length ?? 0,
   });
 
-  if (status === "pending") {
+  if (status === "loading") {
     return (
       <div className="flex flex-col flex-1 justify-center items-center">
         <Loader2Icon className="h-7 w-7 text-zinc-500 animate-spin my-4" />
